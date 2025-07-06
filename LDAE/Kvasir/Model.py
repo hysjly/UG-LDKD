02dd63a0795bf19a926628b165b53abfc991e537
unknown <2500875860@qq.com>
2025-07-06 15:46:11 +0800

    Update

--- LDAE/Kvasir/Model.py
+++ LDAE/Kvasir/Model.py
@@ -329,19 +329,31 @@ class Encoder(nn.Module):
                                  Swish(),
                                  nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=True),
                                  Swish(),
+            			 ResBlock(64), 
                                  nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=True),
                                  Swish(),
+            			 ResBlock(128), 
                                  nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=True),
                                  Swish(),
-                                 nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=True),
-                                 Swish(),
-                                 nn.Flatten(),
-                                 nn.Linear(256*16*16, 1024),
-                                 Swish(),
-                                 nn.Linear(1024, 1)
+            			 ResBlock(128), 
+                                 SpatialAttention(128), 
+                                 nn.Conv2d(128, 256, 3, padding=1),
+                                 Swish()
                                  )
+        self.feature_proj = nn.Sequential(
+            nn.Conv2d(256, d, 1),  
+            Swish(),
+            AdaptivePool2d((16, 16))  
+         )
+        self.energy_head = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 16 * 16, 1024),
+            Swish(),
+            nn.Linear(1024, 1)
+        )
 
     def forward(self, x):
         x = self.net(x).squeeze(dim=-1)
+	x = self.feature_proj(x)
+        x = self.energy_head(x)
         return x
-    
